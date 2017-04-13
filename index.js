43ab8695bb6ff82b38ea0fbf3c9542196614d8e7
osk2 <sc60714@gmail.com>
2017-04-13 14:39:44 +0800

    Complete backend
    - Provide HTTPS
    - Save uploaded blob to wav
    - Provide analyzed contour JSON data

--- index.js
+++ index.js
@@ -1,49 +1,52 @@
 const exec = require('child_process').execSync;
 const express = require('express');
+const https = require('https');
+const csvjson = require('csvjson');
 const fs = require('fs');
 const app = express();
 const config = {
   projectName: 'pitch-analyzer',
   praatPath: function () { return '/var/www/' + this.projectName + '/praat/'},
-  sampleFilename: 'sample'
+  sampleFilename: 'sample',
+  uploadPath: 'upload/',
+  ca: 'ssl/ca_bundle.crt',
+  key: 'ssl/private.key',
+  cert: 'ssl/certificate.crt'
+};
+const sslOptions = {
+  ca: fs.readFileSync(config.ca),
+  key: fs.readFileSync(config.key),
+  cert: fs.readFileSync(config.cert)
 };
 
 const analyzePitch = (filename) => {
-  const path = '/var/www/' + config.projectName + '/upload/' + file;
-  // Extract pitch and intensity from wav
-  exec('praat --run ' + config.praatPath + 'extractPitchIntensity.praat "' + config.praatPath + '" "' + path + '"', {stdio:[0,1,2]});
-  // Analyze pitch and intensity from output
-  exec('praat --run ' + config.praatPath + 'pitchIntensityScript.praat "' + config.praatPath + '" "' + path + '"', {stdio:[0,1,2]});
+  const filePath = '/var/www/' + config.projectName + '/upload/';
+  const praatPath = config.praatPath();
+
+  exec('praat --run ' + praatPath + 'extractPitchIntensity.praat "' + filePath + '" "' + filename + '"', {stdio:[0,1,2]});
+  exec('praat --run ' + praatPath + 'pitchIntensityScript.praat "' + filePath + '" "' + filename + '"', {stdio:[0,1,2]});
   const data = fs.readFileSync('upload/' + filename + '.graph', { encoding : 'utf8'});
-  const options = {
-    delimiter: ',',
+  return csvjson.toColumnArray(data, {
+    delimiter: '\t',
     quote: '"'
-  };
-  return csvjson.toColumnArray(data, options);
+  });
 }
 
 app.use(express.static('public'));
-app.use(express.bodyParser({uploadDir:'./'}));
-
 app.post('/upload', (req, res) => {
-  const tmp_path = req.files.audio.name;
-  const target_path = '/var/www/' + config.projectName + '/upload/';
-  const fileReader = new FileReader();
+  const date = new Date();
+  const filename = date.getTime() + '.wav';
 
-  fileReader.readAsArrayBuffer(blob);
-  fileReader.onload = function () {
-    const date = new Date();
-    const filename = date.getTime() + '.wav';
-
-    fs.writeFileSync(target_path + filename, Buffer(new Uint8Array(this.result)));
-    res.json({success: true, file: filename});
-  };
+  req.pipe(fs.createWriteStream(config.uploadPath + filename))
+    .on('error', (e) => res.status(500).json({success: false, message: e.message}).end())
+    .on('close', () => res.json({success: true, file: date.getTime()}))
 });
 
 app.get('/analyze/:filename', (req, res) => {
   res.json(analyzePitch(req.params.filename));
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+https.createServer(sslOptions, app).listen(3000, function() {
+  console.log('Listening on port 3000');
 });
+
